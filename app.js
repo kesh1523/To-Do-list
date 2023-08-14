10f278327595be6d438e49039ef39be309611461
Kesh Pratap Singh <kpsingh0152@gmail.com>
2023-08-14 22:16:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017", {
+mongoose.connect("mongodb+srv://kpsingh0152:<password>@cluster1.09b8e7p.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
